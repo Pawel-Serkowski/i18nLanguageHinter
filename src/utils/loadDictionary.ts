9df35103d0c1ad4fd70083b786dad0272d0a2bb6
Paweł Serkowski <paw.serkowski@gmail.com>
2026-05-20 00:21:55 +0200

    Added file watcher to handle changing dictionary

--- src/utils/loadDictionary.ts
+++ src/utils/loadDictionary.ts
@@ -4,36 +4,45 @@ import * as path from "path";
 
 import { forceReloadInlayHints, dictionaryMap } from "../extension";
 
+let fileWatcher: vscode.FileSystemWatcher;
+
 export function loadDictionaryFile(filePath: string) {
-    try {
-        const fileContent = fs.readFileSync(filePath, "utf-8");
-        const parsed = JSON.parse(fileContent);
+	try {
+		const fileContent = fs.readFileSync(filePath, "utf-8");
+		const parsed = JSON.parse(fileContent);
 
-        dictionaryMap.clear();
+		dictionaryMap.clear();
 
-        for (const [key, value] of Object.entries(parsed)) {
-            if (typeof value === "string") {
-                dictionaryMap.set(key, value);
-            }
-        }
-        forceReloadInlayHints();
+		for (const [key, value] of Object.entries(parsed)) {
+			if (typeof value === "string") {
+				dictionaryMap.set(key, value);
+			}
+		}
+		forceReloadInlayHints();
 
-        vscode.window.showInformationMessage(`Success! Loaded ${dictionaryMap.size} language keys.`);
-    } catch (error) {
-        vscode.window.showErrorMessage("Failed to load dictionary file: " + error);
-    }
+		if (fileWatcher) { fileWatcher.dispose(); }
+
+		fileWatcher = vscode.workspace.createFileSystemWatcher(filePath);
+		fileWatcher.onDidChange(() => {
+			loadDictionaryFile(filePath);
+		});
+
+		vscode.window.showInformationMessage(`Success! Loaded ${dictionaryMap.size} language keys.`);
+	} catch (error) {
+		vscode.window.showErrorMessage("Failed to load dictionary file: " + error);
+	}
 }
 
 export function pluginSetup(context: vscode.ExtensionContext) {
-    const savedFolder = context.workspaceState.get<string>("i18nhinter.dictionaryFolder");
-    if (!savedFolder) return;
+	const savedFolder = context.workspaceState.get<string>("i18nhinter.dictionaryFolder");
+	if (!savedFolder) return;
 
-    let activeFile = context.workspaceState.get<string>("i18nhinter.activeFile");
-    if (!activeFile) {
-        activeFile = path.join(savedFolder, "en-US.json");
-    }
+	let activeFile = context.workspaceState.get<string>("i18nhinter.activeFile");
+	if (!activeFile) {
+		activeFile = path.join(savedFolder, "en-US.json");
+	}
 
-    if (fs.existsSync(activeFile)) {
-        loadDictionaryFile(activeFile);
-    }
+	if (fs.existsSync(activeFile)) {
+		loadDictionaryFile(activeFile);
+	}
 }
